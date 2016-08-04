a08dbda5c36ce6526cf854ec40a9515143e98147
Olivia Hinton <hinton.olivia@gmail.com>
2016-08-03 22:49:50 -0700

    add working temp measurement

--- js/temperature-interface.js
+++ js/temperature-interface.js
@@ -2,15 +2,24 @@ var Temperature = require('./../js/temperature.js').temperatureModule;
 
 var apiKey = require('./../.env').apiKey;
 
-var displayTemp = function(city, tempData) {
-  $('.showTemp').text("The temperature in " + city + " is " + tempData);
-};
-
 $(document).ready(function() {
-  var currentWeatherObject = new Temperature();
   $('#cityTemp').click(function() {
     var city = $('#location-temp').val();
     $('#location-temp').val("");
-    currentWeatherObject.getWeather(city, displayTemp);
+    var unitType = $("#temp-unit").val();
+    $.get("http://api.openweathermap.org/data/2.5/weather?q=" + city + '&appid=' + apiKey).then(function(response){
+    var currentWeatherObject = new Temperature(response.main.temp);
+    var tempOutput;
+    if (unitType === "Fahrenheit"){
+      tempOutput = currentWeatherObject.fahrenheit
+    } else {
+      tempOutput = currentWeatherObject.celsius
+    }
+
+    $(".showTemp").text("The temperature in " + city + " is " + tempOutput + " degrees!");
+
+    }).fail(function(error){
+   $("#error-message").text(error.responseJSON.message);
     });
   });
+});
